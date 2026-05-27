38380674ea5081923e48bf2ed6aa61d478a1e8d9
Sailaja-Kalle <kallesailaja83@gmail.com>
2026-05-27 10:11:50 +0530

    Fix API key loading for deployment

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,3 +1,4 @@
+﻿import os
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 
@@ -5,9 +6,11 @@ load_dotenv()
 
 def load_llm():
     print("[INFO] Loading Groq LLM...")
+    api_key = os.environ.get("GROQ_API_KEY")
     llm = ChatGroq(
         model="llama-3.1-8b-instant",
-        temperature=0.3
+        temperature=0.3,
+        api_key=api_key
     )
     print("[INFO] LLM loaded.")
     return llm
